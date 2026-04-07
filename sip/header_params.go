d9f2d85332a23d6d1d48b37fc3b03df3e23ec26c
Alex <alex.migolin@livekit.io>
2026-04-07 13:55:58 -0700

    suggesting update

--- sip/header_params.go
+++ sip/header_params.go
@@ -10,6 +10,8 @@ const abnfWs = " \t"
 // HeaderParams are key value params. They do not provide order by default due to performance reasons
 type HeaderParams = sipgo.HeaderParams
 
+type HeaderKV = sipgo.HeaderKV
+
 func NewParams() HeaderParams {
 	return sipgo.NewParams()
 }
